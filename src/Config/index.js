e89fd9e0c62895659946b16857eab3cd7041162b
manoharkr001 <133251335+manoharkr001@users.noreply.github.com>
2025-05-12 22:44:21 +0530

    Delete src/Config/index.js

--- src/Config/index.js
+++
@@ -1,9 +0,0 @@
-// import Function from './Functions/Functions'
-import Router from './Router/Router'
-// import Firebase from './Firebase/Firebase'
-
-export{
-    // Function,
-    Router
-    // Firebase
-}
